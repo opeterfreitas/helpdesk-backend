1d9f3f4dd43757ad2626467d8424ef599f0907ad
Peter Freitas <opeterfreitas@gmail.com>
2023-01-05 11:27:33 -0300

    Anotações JPA para classes de domínio

--- src/main/java/com/opeterfreitas/helpdesk/domain/Tecnico.java
+++ src/main/java/com/opeterfreitas/helpdesk/domain/Tecnico.java
@@ -1,17 +1,26 @@
 package com.opeterfreitas.helpdesk.domain;
 
+import com.opeterfreitas.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Tecnico extends Pessoa{
+    private static final long serialVersionUID = 1L;
 
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Chamado> getChamados() {
