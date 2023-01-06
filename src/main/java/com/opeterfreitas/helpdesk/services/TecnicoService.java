fb5a9b2b1ae23488f05d1efebb82737c6c9f9305
Peter Freitas <opeterfreitas@gmail.com>
2023-01-06 12:07:38 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/opeterfreitas/helpdesk/services/TecnicoService.java
+++ src/main/java/com/opeterfreitas/helpdesk/services/TecnicoService.java
@@ -2,6 +2,7 @@ package com.opeterfreitas.helpdesk.services;
 
 import com.opeterfreitas.helpdesk.domain.Tecnico;
 import com.opeterfreitas.helpdesk.repositories.TecnicoRepository;
+import com.opeterfreitas.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
     }
 
 
