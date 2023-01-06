fa79e4e8f4b8e2bdec6f9b2dfe243e2023b75a39
Peter Freitas <opeterfreitas@gmail.com>
2023-01-06 09:19:59 -0300

    Perfil de desenvolvimento criado

---
+++ src/main/java/com/opeterfreitas/helpdesk/config/DevConfig.java
@@ -0,0 +1,27 @@
+package com.opeterfreitas.helpdesk.config;
+
+import com.opeterfreitas.helpdesk.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
+    @Bean
+    public boolean instanciaDb() {
+        if (value.equals("create")) {
+            this.dbService.instanciaDB();
+        }
+        return false;
+    }
+}
