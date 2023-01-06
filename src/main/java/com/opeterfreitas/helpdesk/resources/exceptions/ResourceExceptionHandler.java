ad1e900d75d8a0449a53c57a183b2b5334051482
Peter Freitas <opeterfreitas@gmail.com>
2023-01-06 14:41:51 -0300

    Exceção para CPF e E-mail já cadastrados

--- src/main/java/com/opeterfreitas/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/opeterfreitas/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.opeterfreitas.helpdesk.resources.exceptions;
 
+import com.opeterfreitas.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.opeterfreitas.helpdesk.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,4 +17,12 @@ public class ResourceExceptionHandler {
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Object Not Found", ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+                                                                 HttpServletRequest request) {
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Violação de dados", ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
 }
--- src/main/java/com/opeterfreitas/helpdesk/services/TecnicoService.java
+++ src/main/java/com/opeterfreitas/helpdesk/services/TecnicoService.java
@@ -1,8 +1,11 @@
 package com.opeterfreitas.helpdesk.services;
 
+import com.opeterfreitas.helpdesk.domain.Pessoa;
 import com.opeterfreitas.helpdesk.domain.Tecnico;
 import com.opeterfreitas.helpdesk.domain.dtos.TecnicoDTO;
+import com.opeterfreitas.helpdesk.repositories.PessoaRepository;
 import com.opeterfreitas.helpdesk.repositories.TecnicoRepository;
+import com.opeterfreitas.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.opeterfreitas.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,10 +19,12 @@ public class TecnicoService {
 
     @Autowired
     private TecnicoRepository repository;
+    @Autowired
+    private PessoaRepository pessoaRepository;
 
-    public Tecnico findById(Integer id){
+    public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
     }
 
     public List<Tecnico> findAll() {
@@ -29,7 +34,19 @@ public class TecnicoService {
     @Transactional
     public Tecnico create(TecnicoDTO objDTO) {
         objDTO.setId(null);
+        validaPorCpfEEmail(objDTO);
         Tecnico newObj = new Tecnico(objDTO);
         return repository.save(newObj);
+    }
+
+    private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+        if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+        }
+        obj = pessoaRepository.findByEmail(objDTO.getEmail());
+        if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+        }
     }
 }
