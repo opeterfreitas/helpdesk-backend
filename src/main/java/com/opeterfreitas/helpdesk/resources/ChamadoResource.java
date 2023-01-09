00f3ce4f6acbc6c6c081d5391a2297326e0ff832
Peter Freitas <opeterfreitas@gmail.com>
2023-01-09 14:03:46 -0300

    findById para Chamado

---
+++ src/main/java/com/opeterfreitas/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,25 @@
+package com.opeterfreitas.helpdesk.resources;
+
+import com.opeterfreitas.helpdesk.domain.Chamado;
+import com.opeterfreitas.helpdesk.domain.dtos.ChamadoDTO;
+import com.opeterfreitas.helpdesk.services.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chamados")
+public class ChamadoResource {
+
+    @Autowired
+    private ChamadoService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+        Chamado obj = service.findById(id);
+        return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+}
